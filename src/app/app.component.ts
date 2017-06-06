0b088757aa7843ea18ed31da40cc85ce560f26b6
schemr <schemryj@gmail.com>
2017-06-06 15:53:56 +0900

    컴포넌트 분리 3. Todo-list-footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,7 @@ import { TodoDataService } from './todo-data.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [TodoDataService]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,24 @@ import { AppComponent } from './app.component';
 import { TodoListHeaderComponent } from './todo-list-header/todo-list-header.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoListItemComponent } from './todo-list-item/todo-list-item.component';
+import { TodoListFooterComponent } from './todo-list-footer/todo-list-footer.component';
+
+import { TodoDataService } from './todo-data.service'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoListHeaderComponent,
     TodoListComponent,
-    TodoListItemComponent
+    TodoListItemComponent,
+    TodoListFooterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [TodoDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
