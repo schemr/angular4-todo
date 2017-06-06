d553b051c79df05e2ded0a096ac2f703bc82a2fb
schemr <schemryj@gmail.com>
2017-06-06 15:18:28 +0900

    App Component 구현, 테스트 수정

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
+import { Todo } from './todo'; 
+import { TodoDataService } from './todo-data.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
 export class AppComponent {
-  title = 'app works!';
+
+  newTodo: Todo = new Todo();
+  todos: Todo[] = [];
+  
+  constructor(private todoDataService: TodoDataService){}
+
+  addTodo(){
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  toggleTodoComplete(todo){
+    this.todoDataService.toggleTodoComplete(todo)
+  }
+
+  removeTodo(todo){
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get Todos(){
+    console.log(this.todoDataService.getAllTodos())
+    return this.todoDataService.getAllTodos();
+  }
 }
